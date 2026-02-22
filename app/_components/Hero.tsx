077a05be0841574eca4d4e24a7b371114dd0eb47
Oyunjargal17 <oyunjargal.teacher@gmail.com>
2026-02-22 12:19:45 +0800

    02.22 gertee

---
+++ app/_components/Hero.tsx
@@ -0,0 +1,16 @@
+export const Hero = () => {
+  return (
+    <div>
+      <div className="flex gap-4">
+        <img
+          src="https://preview-product-requirements-document-kzmq96wpkm2qmnnjxfm8.vusercontent.net/"
+          alt="logo"
+        />
+        <div>
+          <h1>Дхамма-Махаана</h1>
+          <h3>Випашяна Бясалгалын Төв</h3>
+        </div>
+      </div>
+    </div>
+  );
+};
