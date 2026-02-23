3e38cf4fbe2033f3e1dd4ba03855f0017b7ced70
Oyunjargal17 <oyunjargal.teacher@gmail.com>
2026-02-23 18:52:49 +0800

    phone header

--- app/_components/Hero.tsx
+++ app/_components/Hero.tsx
@@ -1,16 +1,40 @@
+import { ArrowRight, Menu, MenuIcon } from 'lucide-react';
+
 export const Hero = () => {
   return (
     <div>
-      <div className="flex gap-4">
-        <img
-          src="https://preview-product-requirements-document-kzmq96wpkm2qmnnjxfm8.vusercontent.net/"
-          alt="logo"
-        />
-        <div>
-          <h1>Дхамма-Махаана</h1>
+      <div className="flex gap-4 bg-[#eeece9] justify-between">
+        <span className="font-bold p-4">ॐ</span>
+        <div  className='flex '>
+          <div className='hidden md:block md:justify-between'>
+ <h1>Дхамма-Махаана</h1>
           <h3>Випашяна Бясалгалын Төв</h3>
+          </div>
+         
+          <div>
+<p className='p-4'><MenuIcon /></p>
+          </div>
+          
+          
+
+    
+  
+
+
+
         </div>
       </div>
+     <div>
+      <div className='bg-[#eeece9] w-fit flex flex-col mt-10 text-center p-2'>
+        <h3>Монгол дахь Випашяна бясалгалын албан ёсны төв</h3>
+        <h1>Дхамма-Махаана</h1>
+        <h1>Випашяна бол Энэтхэгийн эртний бясалгалын нэг техник бөгөөд оюун санааны бохирдлыг арилгаж, дээд аз жаргалыг олж авах арга зам юм.</h1>
+      </div>
+      <div>
+        <button className='border flex'>Бүртгүүлэх <ArrowRight /></button>
+        <button className='border'>Дэлгэрэнгүй</button>
+      </div>
+     </div>
     </div>
   );
 };
