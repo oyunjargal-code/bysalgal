66481c7835dbc5bbace0aeed920398147feb49a7
Oyunjargal17 <oyunjargal.teacher@gmail.com>
2026-03-22 23:38:41 +0800

    03.22 gertee

--- app/_components/Hero.tsx
+++ app/_components/Hero.tsx
@@ -11,6 +11,13 @@ export const Hero = () => {
           <h3>Випашяна Бясалгалын Төв</h3>
         </div>
       </div>
+      <div>
+        <h1>end ih biy bna</h1>
+      </div>
+      <div>
+        <h5>end medeelel bna</h5>
+      </div>
+      <div>end holboo barih heseg bna</div>
     </div>
   );
 };
